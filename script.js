d045baad4234ff87d698b750d5cfcbf3ec58c5d9
Poorva-Jain1 <poorvajainnagod@gmail.com>
2025-07-16 16:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ function isSameType(value1, value2) {
     return true;
   }
 
-  if (Number.isNaN(value1) || Number.isNaN(value2)) {
-    return false;
-  }
-
+  // Compare types
   return typeof value1 === typeof value2;
-}
  
 }
 
